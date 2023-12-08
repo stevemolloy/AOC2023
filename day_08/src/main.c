7b389888e70a61463fb328eecfa5961bb50a60b0
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-08 12:51:45 +0100

    Hashmap for part 1

--- day_08/src/main.c
+++ day_08/src/main.c
@@ -5,12 +5,19 @@
 #include <string.h>
 
 #include "aoc_lib.h"
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
 
 typedef struct {
   char *name;
   char *left;
   char *right;
 } Node;
+
+typedef struct {
+  char *key;
+  Node value;
+} Map_Element;
 
 size_t gcd(size_t a, size_t b) {
     if (b == 0) return a;
@@ -62,73 +69,77 @@ int main(int argc, char **argv) {
 
   size_t num_nodes = num_lines - 2;
 
-  Node *nodes = calloc(num_nodes, sizeof(Node));
+  Map_Element *map = NULL;
+
+  // Node *nodes = calloc(num_nodes, sizeof(Node));
   for (size_t i=0; i<num_nodes; i++) {
     char *cursor = lines[i+2];
-    nodes[i] = (Node) {0};
-    nodes[i].name = cursor;
+    Node node = (Node) {0};
+    node.name = cursor;
+    cursor += 3;
+    *cursor = '\0';
     advance_to_char(&cursor, '(');
     cursor++;
-    nodes[i].left = cursor;
-    cursor += 3 + 2;
-    nodes[i].right = cursor;
+    node.left = cursor;
+    cursor += 3;
+    *cursor = '\0';
+    cursor += 2;
+    node.right = cursor;
+    cursor += 3;
+    *cursor = '\0';
+
+    shput(map, node.name, node);
   }
 
-  size_t current_node = get_node_by_name(nodes, num_nodes, "AAA");
+  // size_t current_node = get_node_by_name(nodes, num_nodes, "AAA");
+  Node current_node = shget(map, "AAA");
   size_t steps_taken = 0;
   for (size_t i=0; ; i = (i+1) % instruction_count) {
-    Node n = nodes[current_node];
+    Node n = current_node;
     if (strncmp(n.name, "ZZZ", 3) == 0) {
       printf("Done!\n");
       break;
     }
-    if (lines[0][i] == 'L') {
-      current_node = get_node_by_name(nodes, num_nodes, n.left);
-    } else {
-      current_node = get_node_by_name(nodes, num_nodes, n.right);
-    }
+    if (lines[0][i] == 'L') current_node = shget(map, n.left);
+    else current_node = shget(map, n.right);
     steps_taken++;
   }
 
   printf("Answer to part 1 = %zu\n", steps_taken);
 
-  size_t num_start_points = count_starting_nodes(nodes, num_nodes);
-
-  size_t *active_nodes = calloc(num_start_points, sizeof(size_t));
-  size_t an_index = 0;
-  for (size_t i=0; i<num_nodes; i++) {
-    Node n = nodes[i];
-    if (n.name[2] == 'A') active_nodes[an_index++] = i;
-  }
-
-  size_t *steps = calloc(num_start_points, sizeof(size_t));
-
-  for (size_t i=0; i<num_start_points; i++) {
-    size_t steps_taken = 0;
-    for (size_t j=0; ; j = (j+1) % instruction_count) {
-      char instruction = lines[0][j];
-      Node n = nodes[active_nodes[i]];
-      if (n.name[2] == 'Z') {
-        steps[i] = steps_taken;
-        break;
-      }
-      if (instruction == 'L') {
-        active_nodes[i] = get_node_by_name(nodes, num_nodes, n.left);
-      } else {
-        active_nodes[i] = get_node_by_name(nodes, num_nodes, n.right);
-      }
-      steps_taken++;
-    }
-  }
-
-  size_t ans = lcm(steps, num_start_points);
-  printf("Answer to part 2 = %zu\n", ans);
-
-  free(buffer);
-  free(lines);
-  free(nodes);
-  free(active_nodes);
-  free(steps);
+  // size_t num_start_points = count_starting_nodes(nodes, num_nodes);
+  //
+  // size_t *active_nodes = calloc(num_start_points, sizeof(size_t));
+  // size_t an_index = 0;
+  // for (size_t i=0; i<num_nodes; i++) {
+  //   if (nodes[i].name[2] == 'A') active_nodes[an_index++] = i;
+  // }
+  //
+  // size_t *steps = calloc(num_start_points, sizeof(size_t));
+  //
+  // for (size_t i=0; i<num_start_points; i++) {
+  //   size_t steps_taken = 0;
+  //   for (size_t j=0; ; j = (j+1) % instruction_count) {
+  //     char instruction = lines[0][j];
+  //     Node n = nodes[active_nodes[i]];
+  //     if (n.name[2] == 'Z') {
+  //       steps[i] = steps_taken;
+  //       break;
+  //     }
+  //     if (instruction == 'L') active_nodes[i] = get_node_by_name(nodes, num_nodes, n.left);
+  //     else active_nodes[i] = get_node_by_name(nodes, num_nodes, n.right);
+  //     steps_taken++;
+  //   }
+  // }
+  //
+  // size_t ans = lcm(steps, num_start_points);
+  // printf("Answer to part 2 = %zu\n", ans);
+  //
+  // free(buffer);
+  // free(lines);
+  // free(nodes);
+  // free(active_nodes);
+  // free(steps);
 
   return 0;
 }
