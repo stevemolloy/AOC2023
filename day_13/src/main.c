03021db9c8804e0f999c541558c25b880b9db90b
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-13 10:34:03 +0100

    Starting day 13

---
+++ day_13/src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aoc_lib.h"
+
+int main(void) {
+  char *file_path = "./test_input.txt";
+  // char *file_path = "./real_input.txt";
+  char *buffer;
+  char **lines;
+
+  size_t num_lines = read_entire_file_to_lines(file_path, &buffer, &lines);
+
+  return 0;
+}
