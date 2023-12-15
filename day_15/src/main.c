20d1f10eb8476faf529b33f5b4e2fd662e4b39fb
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-15 09:27:40 +0100

    Part 1 done

--- day_15/src/main.c
+++ day_15/src/main.c
@@ -4,16 +4,37 @@
 
 #include "aoc_lib.h"
 
+size_t hash_next_char(char c, size_t curr_hash) {
+  curr_hash += c;
+  curr_hash = (curr_hash%256) * 17;
+  curr_hash %= 256;
+  return curr_hash;
+}
+
 int main(void) {
-  char *file_path = "./test_input.txt";
-  // char *file_path = "./real_input.txt";
-  char **lines;
-  char *buffer;
+  // char *file_path = "./test_input.txt";
+  char *file_path = "./real_input.txt";
 
-  size_t num_lines = read_entire_file_to_lines(file_path, &buffer, &lines);
+  char *contents = read_entire_file(file_path);
+  // char *contents = "rn=1";
+  printf("%s\n", contents);
 
-  free(lines);
-  free(buffer);
+  char *cursor = contents;
+  size_t hash = 0;
+  size_t total = 0;
+  while (*cursor) {
+    if (*cursor == '\n') {
+    } else if (*cursor == ',') {
+      printf("%zu\n", hash);
+      total += hash;
+      hash = 0;
+    } else {
+      hash = hash_next_char(*cursor, hash);
+    }
+    cursor++;
+  }
+  total += hash;
+  printf("Answer to part 1 = %zu\n", total);
 
   return 0;
 }
