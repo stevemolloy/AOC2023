03f303fdfffd8d45e599da1681faa9429a3fc616
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-12-18 11:12:43 +0100

    Part 1 done

--- day_18/src/main.c
+++ day_18/src/main.c
@@ -9,14 +9,17 @@
 #include "aoc_lib.h"
 
 typedef enum {
-  RIGHT = 0,
+  NONE = 0,
+  RIGHT,
   LEFT,
   UP,
   DOWN,
-} Direction;
+  FILLED,
+} SquareType;
 
 int main(void) {
-  char *file_path = "./test_input.txt";
+  // char *file_path = "./test_input.txt";
+  char *file_path = "./real_input.txt";
   char *buffer;
   char **lines;
 
@@ -56,19 +59,18 @@ int main(void) {
    if (-vertical_loc > max_up) max_up = -vertical_loc;
   }
 
-  size_t grid_rows = max_down - max_up + 1;
-  size_t grid_cols = max_right - max_left + 1;
+  size_t grid_rows = 2 * (max_down - max_up + 1);
+  size_t grid_cols = 2 * (max_right - max_left + 1);
 
-  bool *dig_grid = malloc(grid_rows*grid_cols*sizeof(bool));
+  SquareType *dig_grid = malloc(grid_rows*grid_cols*sizeof(SquareType));
   for (size_t j=0; j<grid_rows; j++) {
     for (size_t i=0; i<grid_cols; i++) {
-      dig_grid[j*grid_cols + i] = false;
+      dig_grid[j*grid_cols + i] = NONE;
     }
   }
 
   size_t curr_x = max_left;
   size_t curr_y = max_up;
-  dig_grid[curr_y*grid_cols + curr_x] = true;
 
   for (size_t r=0; r<num_rows; r++) {
     char *line = lines[r];
@@ -80,7 +82,7 @@ int main(void) {
       printf("Moving R by %zu\n", next_val);
       for (size_t i=0; i<next_val; i++) {
         curr_x++;
-        dig_grid[curr_y*grid_cols + curr_x] = true;
+        dig_grid[curr_y*grid_cols + curr_x] = RIGHT;
       }
     }
     else if (command == 'L') {
@@ -89,7 +91,7 @@ int main(void) {
       printf("Moving L by %zu\n", next_val);
       for (size_t i=0; i<next_val; i++) {
         curr_x--;
-        dig_grid[curr_y*grid_cols + curr_x] = true;
+        dig_grid[curr_y*grid_cols + curr_x] = LEFT;
       }
     }
     else if (command == 'U') {
@@ -98,7 +100,7 @@ int main(void) {
       printf("Moving U by %zu\n", next_val);
       for (size_t i=0; i<next_val; i++) {
         curr_y--;
-        dig_grid[curr_y*grid_cols + curr_x] = true;
+        dig_grid[curr_y*grid_cols + curr_x] = UP;
       }
     }
     else if (command == 'D') {
@@ -107,7 +109,30 @@ int main(void) {
       printf("Moving D by %zu\n", next_val);
       for (size_t i=0; i<next_val; i++) {
         curr_y++;
-        dig_grid[curr_y*grid_cols + curr_x] = true;
+        dig_grid[curr_y*grid_cols + curr_x] = DOWN;
+      }
+    }
+  }
+
+  for (size_t j=1; j<grid_rows-1; j++) {
+    for (size_t i=1; i<grid_cols-1; i++) {
+      if (dig_grid[j*grid_cols + i] != NONE) continue;
+      if (dig_grid[j*grid_cols + i-1] == UP || dig_grid[j*grid_cols + i+1] == DOWN) {
+        dig_grid[j*grid_cols + i] = FILLED;
+      }
+      if (dig_grid[(j-1)*grid_cols + i] == RIGHT || dig_grid[(j+1)*grid_cols + i] == LEFT) {
+        dig_grid[j*grid_cols + i] = FILLED;
+      }
+    }
+  }
+
+  for (size_t j=1; j<grid_rows-1; j++) {
+    for (size_t i=1; i<grid_cols-1; i++) {
+      if (dig_grid[j*grid_cols + i] == NONE && dig_grid[j*grid_cols + i-1] == FILLED) {
+        dig_grid[j*grid_cols + i] = FILLED;
+      }
+      if (dig_grid[j*grid_cols + i] == NONE && dig_grid[(j-1)*grid_cols + i] == FILLED) {
+        dig_grid[j*grid_cols + i] = FILLED;
       }
     }
   }
@@ -115,13 +140,31 @@ int main(void) {
   size_t path_length = 0;
   for (size_t j=0; j<grid_rows; j++) {
     for (size_t i=0; i<grid_cols; i++) {
-      printf("%c", dig_grid[j*grid_cols + i] ? '#' : '.');
-      if (dig_grid[j*grid_cols + i]) path_length++;
+      switch (dig_grid[j*grid_cols + i]) {
+        case RIGHT:
+          putc('>', stdout);
+          break;
+        case LEFT:
+          putc('<', stdout);
+          break;
+        case UP:
+          putc('^', stdout);
+          break;
+        case DOWN:
+          putc('v', stdout);
+          break;
+        case FILLED:
+          putc('#', stdout);
+          break;
+        case NONE:
+          putc('.', stdout);
+      }
+      if (dig_grid[j*grid_cols + i] != NONE) path_length++;
     }
     printf("\n");
   }
 
-  printf("Path length = %zu\n", path_length);
+  printf("Answer to part 1 = %zu\n", path_length);
 
   return 0;
 }
